8deb01f2415549d7f87eedac1213fedeeb3ed103
seriouscatsoserious <121734574+seriouscatsoserious@users.noreply.github.com>
2024-07-28 17:20:03 -0300

    Make nav transparent, seperate nav from app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, NavLink, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import NavBar from './components/NavBar';
 import Nodes from './components/Nodes';
 import Wallet from './components/Wallet';
 import Tools from './components/Tools';
 import Settings from './components/Settings';
 import Other from './components/Other';
 import { ThemeProvider, useTheme } from './contexts/ThemeContext';
-import ThemeToggle from './components/ThemeToggle';
 
 function AppContent() {
   const { isDarkMode } = useTheme();
@@ -15,17 +15,7 @@ function AppContent() {
   return (
     <Router>
       <div className={`App ${isDarkMode ? 'dark' : 'light'}`}>
-      <nav>
-  <ul>
-    <li><NavLink to="/" end>Nodes</NavLink></li>
-    <li><NavLink to="/wallet">Wallet</NavLink></li>
-    <li><NavLink to="/tools">Tools</NavLink></li>
-    <li><NavLink to="/settings">Settings</NavLink></li>
-    <li><NavLink to="/other">Other</NavLink></li>
-  </ul>
-  <ThemeToggle />
-</nav>
-
+        <NavBar />
         <Routes>
           <Route path="/" element={<Nodes />} />
           <Route path="/wallet" element={<Wallet />} />
---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import ThemeToggle from './ThemeToggle';
+import styles from './NavBar.module.css';
+
+const NavBar = () => {
+  console.log('NavBar rendering, styles:', styles);
+  
+  return (
+    <nav className={styles.nav}>
+      <ul className={styles.navList}>
+        <li><NavLink to="/" end className={styles.navLink}>Nodes</NavLink></li>
+        <li><NavLink to="/wallet" className={styles.navLink}>Wallet</NavLink></li>
+        <li><NavLink to="/tools" className={styles.navLink}>Tools</NavLink></li>
+        <li><NavLink to="/settings" className={styles.navLink}>Settings</NavLink></li>
+        <li><NavLink to="/other" className={styles.navLink}>Other</NavLink></li>
+      </ul>
+      <ThemeToggle />
+    </nav>
+  );
+};
+
+export default NavBar;
