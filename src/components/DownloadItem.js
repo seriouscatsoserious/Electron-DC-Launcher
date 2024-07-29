8925624b5d34a9b4f68dcb36fc735437c9d8cc64
seriouscatsoserious <121734574+seriouscatsoserious@users.noreply.github.com>
2024-07-29 13:30:06 -0300

    Download modal styling

--- src/components/DownloadItem.js
+++ src/components/DownloadItem.js
@@ -1,10 +1,14 @@
-// src/components/DownloadItem.js
 import React from 'react';
 import styles from './DownloadModal.module.css';
 
-const DownloadItem = ({ chainId, status, progress }) => (
+const DownloadItem = ({ chainName, status, progress }) => (
   <div className={styles.downloadItem}>
-    <p>{chainId}: {status} - {progress.toFixed(2)}%</p>
+    <p className={styles.chainName} title={`${chainName}: ${status} - ${progress.toFixed(2)}%`}>
+      {chainName}
+    </p>
+    <p className={styles.statusProgress}>
+      {status} - {progress.toFixed(2)}%
+    </p>
     <div className={styles.progressBarContainer}>
       <div
         className={styles.progressBar}
